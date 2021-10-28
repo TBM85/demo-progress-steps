a8fed2e8edf325bf2ed3c62318834fb1d5f9d881
TBM85 <tballesterm@gmail.com>
2021-10-28 08:36:43 -0400

    Incremental scrolling of steps

---
+++ js/app.js
@@ -0,0 +1,47 @@
+const btnRight = document.querySelector("#btn-right");
+
+const progress = document.querySelector(".progress");
+const steps = document.querySelectorAll(".step");
+
+let stepActive = 1;
+
+// When the button "continue" is clicked,
+// there is a movement of one step to the right
+btnRight.addEventListener("click", () => {
+  stepActive++;
+
+  if (stepActive > steps.length) {
+    stepActive === steps.length;
+  }
+
+  update();
+});
+
+// Update step status
+const update = () => {
+  steps.forEach((step, i) => {
+    if (i < stepActive) {
+      step.classList.add("active");
+    } else {
+      step.classList.remove("active");
+    }
+  });
+
+  let bigSize = window.matchMedia("(min-width: 1200px)");
+
+  resizeWidth(bigSize);
+  bigSize.addEventListener("change", resizeWidth);
+}
+
+// Control what happens when the width size changes
+const resizeWidth = (bigSize) => {
+  const actives = document.querySelectorAll(".active");
+
+  if (bigSize.matches) {
+    progress.style.width = (actives.length - 1) / (steps.length - 1) * 60 + "%";
+  } else {
+    progress.style.width = (actives.length - 1) / (steps.length - 1) * 90 + "%";
+  }
+}
+
+
